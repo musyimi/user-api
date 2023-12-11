209fe8d0f428fe94e0ed4dcdbd2a914edb3ad669
musyimi <musyimi13@gmail.com>
2023-12-11 14:55:33 +0300

    first commit

---
+++ src/test/java/com/musyimi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musyimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
